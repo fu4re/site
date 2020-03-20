5b04c23a049fe120e927acee8338b282cde82fba
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-20 18:36:00 +0300

    Тест ЧПУ

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'hello world';
+
+/**
+ * Автозагрузка классов из директории
+ */
+spl_autoload_register(function (string $className){
+    require_once __DIR__.'\\src\\'.$className.'.php';
+});
+
+$controller = new \App\Controllers\MainController();
+$controller->main();#
