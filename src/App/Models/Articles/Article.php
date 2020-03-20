3e1d5401024eb63761a75908a1a3afc57e62e016
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-20 19:53:05 +0300

    Добавлен класс Статья

---
+++ src/App/Models/Articles/Article.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Models\Articles;
+
+
+use App\Models\Users\User;
+
+class Article
+{
+    private $title;
+    private $text;
+    private $author;
+
+
+    public function __construct(string $title, string $text, User $author)
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+}
