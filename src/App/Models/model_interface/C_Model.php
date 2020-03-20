06ca8d5b340337d46bc454004a5522d92f33024c
fu4re <errozergame@gmail.com>
2020-03-20 21:45:38 +0300

    Создан прототип интерфейса модели. Добавлен класс User.

---
+++ src/App/Models/model_interface/C_Model.php
@@ -0,0 +1,26 @@
+<?php
+interface Model
+{
+    public function loadUsers() {
+        //list func
+    }
+
+    public function loadDelUsers() {
+        //list func
+    }
+
+
+//TODO: check me
+    public function loadUserById() {
+
+    }
+
+    public function delUserById() {
+        //
+    }
+
+    public function changeUserData() {
+
+    }
+
+}
