dbd9c60e27aeb81c582eca294687216fa282af58
fu4re <errozergame@gmail.com>
2020-03-21 18:29:28 +0300

    Объявления переменных

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+
+$site_path = realpath(dirname(__FILE__));
+define ('__SITE_PATH', $site_path);
+
+include 'includes/init.php';
+
+$registry->router = new router($registry);
+
+$registry->router->setPath(__SITE_PATH.'/controller');
+
+$registry->template = new template($registry);
+
