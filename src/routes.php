93b53a16db766fbda625ec562cf6f53f905005cb
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-24 18:04:42 +0300

    Rewrite for alpha

--- src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '~^articles/(\d+)$~' => [ 'get', \App\Controllers\ArticlesController::class, 'view'],
-    '~^articles/(\d+)/edit$~' => [ 'get', \App\Controllers\ArticlesController::class, 'edit'],
-    '~^articles/add$~' => [ 'get', \App\Controllers\ArticlesController::class, 'add'],
-    '~^$~' => [ 'get', \App\Controllers\MainController::class, 'main'],
-];
--- templates/articles/view.php
+++
@@ -1,5 +0,0 @@
-<?php include __DIR__ . '/../header.php'; ?>
-    <h1><?= $article->getTitle() ?></h1>
-    <p><?= $article->getText() ?></p>
-    <p>Автор: <?= $author->getNickname() ?></p>
-<?php include __DIR__ . '/../footer.php'; ?>
