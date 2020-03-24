a1c03d80442f867f7fc321dd96b1b802d022c2bd
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-24 18:48:58 +0300

    Добавление родительского класса для контроллеров

---
+++ src/App/Controllers/AbstractController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Users\User;
+use App\Services\UsersAuthService;
+use App\View\View;
+
+abstract class AbstractController
+{
+    /** @var View */
+    protected $view;
+
+    /** @var User|null */
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = UsersAuthService::getUserByToken();
+        $this->view = new View(__DIR__ . '/../../../templates');
+        $this->view->setVar('user', $this->user);
+    }
+}
