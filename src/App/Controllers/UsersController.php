a0cd98243ceafb6b3497c7523545ef0674465c67
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-25 22:37:15 +0300

    Настроена авторизация по токенам + IPv4

--- src/App/Controllers/UsersController.php
+++ src/App/Controllers/UsersController.php
@@ -4,16 +4,22 @@
 namespace App\Controllers;
 
 use App\Exceptions\InvalidArgumentException;
+use App\Exceptions\NotFoundException;
 use App\Models\Users\User;
 use App\Services\EmailSender;
 use App\Services\UsersActivationService;
+use App\Services\UsersAuthorizationService;
+use http\Header;
 
 class UsersController extends AbstractController
 {
+    /**
+     * Регистрация пользователя
+     * @throws \App\Exceptions\DBException
+     */
     public function signUp()
     {
         if (!empty($_POST)) {
-            var_dump($_POST);
             try {
                 $user = User::signUp($_POST);
             } catch (InvalidArgumentException $e) {
@@ -34,5 +40,52 @@ class UsersController extends AbstractController
         }
 
         $this->view->renderHtml('users/signUp.php');
+    }
+
+    /**
+     * Активация пользователя
+     *
+     * @param int|string $userId
+     * @param string $activationCode
+     *
+     * @throws NotFoundException
+     * @throws \App\Exceptions\DBException
+     */
+    public function activate( $userId, string $activationCode)
+    {
+        $user = User::getById((int)$userId);
+        if ($user == null)
+        {
+            throw new NotFoundException();
+        }
+        $isCodeValid = UsersActivationService::checkActivationCode($user, $activationCode);
+        if ($isCodeValid) {
+            $user->activate();
+            $this->view->renderHtml('users/activateSuccessful.php');
+        }else{
+            throw new NotFoundException();
+        }
+    }
+
+    /**
+     * Авторизация пользователя
+     */
+    public function signIn()
+    {
+        if($this->user != null)
+        {
+            header('Location: /', true, 303); //Пользователь уже авторизовался
+            exit;
+        }
+        if (!empty($_POST)) {
+            try {
+                $this->user = User::login($_POST);
+                UsersAuthorizationService::createToken($this->user);
+            } catch (InvalidArgumentException $e) {
+                $this->view->renderHtml('users/login.php', ['error' => $e->getMessage()]);
+                return;
+            }
+        }
+        $this->view->renderHtml('users/login.php');
     }
 }
--- src/App/Services/UsersAuthorizationService.php
+++ src/App/Services/UsersAuthorizationService.php
@@ -12,8 +12,55 @@ class UsersAuthorizationService
 
     public static function createToken(User $user): void
     {
-        $token = hash('sha384', time());
+        $db = Database::getInstance();
+        $result = $db->query('SELECT * FROM `' . self::TABLE_NAME . '` WHERE `user_id` = :id',
+            ['id' => $user->getId()]
+        );
+
+        if ($result !== null)
+        {
+            foreach ($result as $row)
+            {
+                [ $id, $user_id, $a, $ip_addr] = array_values(get_object_vars($row));
+                if ($ip_addr == $_SERVER['REMOTE_ADDR'])
+                {
+                    $token = self::refreshToken($user_id, $ip_addr);
+                }
+                self::newSession($user->getId(), $_SERVER['REMOTE_ADDR']);
+            }
+        }else{
+            $token = self::generateToken();
+            $db->query('INSERT INTO `'.self::TABLE_NAME.'`( `user_id`, `auth_token`, `ip_addr`) VALUES ()', [
+
+            ]);
+        }
+        self::setCookie($token);
+    }
+
+    private static function setCookie(string $token): void
+    {
         setcookie('token', $token, 0, '/', '', false, true);
+    }
+
+    /**
+     * @param int|string $userId ID пользователя
+     * @param string $ip_addr IPv4 адресс пользователя
+     *
+     * @return string
+     * @throws \App\Exceptions\DBException
+     */
+    private static function refreshToken(int $userId,string  $ip_addr) : string
+    {
+        $db = Database::getInstance();
+        $token = self::generateToken();
+        $db->query('UPDATE `' . self::TABLE_NAME . '`SET `auth_token`=:token WHERE `user_id`=:id AND `ip_addr`=:ip',
+            [
+                'token' => $token,
+                'id' => $userId,
+                'ip' => $ip_addr
+            ]
+        );
+        return $token;
     }
 
     /**
@@ -25,7 +72,7 @@ class UsersAuthorizationService
     public static function getUserByToken(): ?User
     {
         $db = Database::getInstance();
-        $token = 'hello';//$_COOKIE['token'] ?? '';
+        $token = $_COOKIE['token'] ?? '';
 
         if (empty($token)) {
             return null;
@@ -41,24 +88,27 @@ class UsersAuthorizationService
             return null;
         }
 
-        $userId = $result[0]->user_id;
-        $ipAddr = $result[0]->ip_addr;
-        $lastLogin = strtotime($result[0]->lastLogin);
+        [ $id, $userId, $authToken, $ip_addr, $lastLogin] = array_values(get_object_vars($result[0]));
+        $lastLogin = strtotime($lastLogin);
+        $refreshTime = 60*60*24;
+        $expiresAt = 3 * $refreshTime;
 
-        if ($ipAddr !== $_SERVER['REMOTE_ADDR']) {
+        if($authToken == $token && $ip_addr == $_SERVER['REMOTE_ADDR'])
+        {
+            if (time()-$lastLogin > $expiresAt)
+            {
+                self::removeToken($token);
+            }else if (time()-$lastLogin > $refreshTime)
+            {
+                self::setCookie(self::refreshToken($userId, $ip_addr));
+            }
+        }else{
             self::removeToken($token);
             throw new AuthorizationException('Suspicious activity detected');
         }
 
-        /*if (time() - $lastLogin > $config['expires'])
-        {
-            self::removeToken($token);
-            throw new AuthorizationException('Expired session');
-        }*/
-
         $user = User::getById((int)$userId);
-
-        if ($user === null) {
+        if ($user == null) {
             return null;
         }
 
@@ -71,7 +121,7 @@ class UsersAuthorizationService
      *
      * @throws \App\Exceptions\DBException
      */
-    public static function removeToken(string $token)
+    private static function removeToken(string $token)
     {
         unset($_COOKIE['token']);
         $db = Database::getInstance();
@@ -79,5 +129,16 @@ class UsersAuthorizationService
             'DELETE FROM `' . self::TABLE_NAME . '` WHERE `auth_token` = :token',
             [ 'token' => $token ]
         );
+    }
+
+    /**
+     * Функция генерации токена
+     * @param string $algo Алгоритм хеширования
+     *
+     * @return string Токен
+     */
+    private static function generateToken($algo = 'sha384') : string
+    {
+        return hash($algo, time());
     }
 }
