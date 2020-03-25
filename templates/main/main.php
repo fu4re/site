dfefa4f879145abb8bc0814688872d137355d3fa
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-25 22:36:43 +0300

    Добавлена страница входа

--- templates/main/main.php
+++ templates/main/main.php
@@ -1,4 +1,6 @@
-
+<?php include __DIR__ . '/../header.php'; ?>
 
 <a href="/users/login">Sign In</a>
-<a href="/users/register">Sign Up</a>
+<a href="/users/registry">Sign Up</a>
+
+<?php include __DIR__ . '/../footer.php'; ?>
