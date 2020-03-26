426456331d2517f10b4367908c321331db1ba260
Ryabikovalex <ryabikov.alex@gmail.com>
2020-03-26 11:20:54 +0300

    Fix testing

--- tests/autoload.php
+++ tests/autoload.php
@@ -8,5 +8,5 @@ include  'vendor/autoload.php';
 //Загружить приложение
 spl_autoload_register(function (string $className){
     if (substr($className, 0, 3) == "App")
-    require_once __DIR__.'\\..\\src\\'.$className.'.php';
+    require_once __DIR__.'/../src/'.str_ireplace('\\', '/', $className).'.php';
 });
